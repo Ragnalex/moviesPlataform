68efa40e583b5a0c22106b4417f6f2babb4c37df
Ragnalex <bernardo.millapan@gmail.com>
2023-07-18 21:20:03 -0400

    Rutas y login en proceso

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//import routes
+import Login from './routes/login/login';
+
+const App = () => {
+  return(
+    <div className='background-general'> 
+      <div className='bg-modal'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Login/>}></Route>
+          </Routes>
+        </Router>
+      </div>  
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ client/src/routes/login/login.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import './login.css'
+
+const Login = () => {
+    return(
+        <div>
+            Esto es el login
+        </div>
+    )
+    
+}
+
+export default Login;
